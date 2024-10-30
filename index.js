eb160e8bd66df1f5e17ea2465ff106ec56a45548
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-10-30 01:56:39 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (parameter1, parameter2) => {
+    return parameter1/parameter2
+}
+const square = parameter => {
+   return parameter * 2 
+}
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2
+}
